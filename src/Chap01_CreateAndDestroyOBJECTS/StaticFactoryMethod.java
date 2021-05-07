cc006dfbb8ff82cc64845b1ac9dc401e9141a90c
DESKTOP-MNA716V\Admin <tuanlc8899@gmail.com>
2021-05-07 18:20:04 +0700

    no message

---
+++ src/Chap01_CreateAndDestroyOBJECTS/StaticFactoryMethod.java
@@ -0,0 +1,10 @@
+package Chap01_CreateAndDestroyOBJECTS;
+
+public class StaticFactoryMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
